446b924aa92340140d6d7b70408c95902c654a24
John Firebaugh <john.firebaugh@gmail.com>
2015-06-01 14:44:48 -0700

    Try different permutation

--- scripts/ios/test.sh
+++ scripts/ios/test.sh
@@ -8,4 +8,5 @@ xctool \
     -project ./test/ios/ios-tests.xcodeproj \
     -scheme 'Mapbox GL Tests' \
     -sdk iphonesimulator8.3 \
+    -destination 'platform=iOS Simulator,name=iPhone 5s (7.1 Simulator)'
     test
