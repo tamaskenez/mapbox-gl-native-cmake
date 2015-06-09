d486b881f84d17a60f66057d979764294f6a9968
Tamas Kenez <tamas.kenez@kishonti.net>
2015-06-09 22:57:06 +0200

    platforms: replace gmtime_r if MSVC

--- src/mbgl/util/time.cpp
+++ src/mbgl/util/time.cpp
@@ -10,7 +10,11 @@ static const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
 
 std::string rfc1123(std::time_t time) {
     std::tm info;
+#ifdef _MSC_VER
+    gmtime_s(&info, &time);
+#else
     gmtime_r(&time, &info);
+#endif
     char buffer[30];
     snprintf(buffer, 30, "%s, %02d %s %4d %02d:%02d:%02d GMT", week[info.tm_wday], info.tm_mday,
              months[info.tm_mon], 1900 + info.tm_year, info.tm_hour, info.tm_min, info.tm_sec);
