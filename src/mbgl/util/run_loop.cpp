29e53723462608b5ec5c8cc04f50be548462a394
Tamas Kenez <tamas.kenez@kishonti.net>
2015-06-12 15:21:33 +0200

    msvc: compiler bug workaround (run_loop/withMutex)

--- src/mbgl/util/run_loop.cpp
+++ src/mbgl/util/run_loop.cpp
@@ -21,7 +21,15 @@ void RunLoop::withMutex(std::function<void()>&& fn) {
 
 void RunLoop::process() {
     Queue queue_;
+
+#ifdef _MSC_VER
+    // there's some bug in VS2015RC with passing 'this' into the closure
+    // remove this workaround as soon as it gets fixed
+    Queue* this_queue = &this->queue;
+    withMutex([&,this_queue] { queue_.swap(*this_queue); });
+#else
     withMutex([&] { queue_.swap(queue); });
+#endif
 
     while (!queue_.empty()) {
         (*(queue_.front()))();
